f13d2ffce7afa9ff2f891fe37ed415be0a9c95e2
Ben Bean <bbean86@gmail.com>
2021-01-24 15:15:49 -0600

    Add historical rates spec to pairs integration spec

--- spec/integration/pairs_spec.rb
+++ spec/integration/pairs_spec.rb
@@ -108,4 +108,88 @@ describe 'Pairs API' do
       end
     end
   end
+
+  path '/api/v1/pairs/{symbols}/rates' do
+    get 'Returns historical rates in candlestick form for pair from Coinbase Pro' do
+      tags 'Pairs'
+      consumes 'application/json'
+      produces 'application/json'
+
+      parameter name: :symbols,
+                in: :path,
+                type: :string,
+                description: 'indicates the pair to get rates for by its symbols',
+                example: 'BTC-USD'
+
+      parameter name: :limit,
+                in: :query,
+                type: :integer,
+                description: 'indicates the maximum number of records the API should return',
+                example: 10
+
+      parameter name: :cursor,
+                in: :query,
+                type: :string,
+                description: 'contains the cursor for pagination to start from, in Base64',
+                example: CGI.escape(Base64.encode64('before__BTC-USD'))
+
+      parameter name: :sort,
+                in: :query,
+                type: :string,
+                description: 'indicates the property and direction to sort the results on',
+                example: 'time DESC'
+
+      parameter name: :interval,
+                in: :query,
+                type: :integer,
+                description: 'contains an integer indicating rate interval in seconds to build candlesticks from',
+                enum: [60, 300, 900, 3600, 21_600, 86_400],
+                example: 60
+
+      let(:limit) { 10 }
+      let(:cursor) { Base64.encode64("after__#{Time.now.to_i}") }
+      let(:sort) { 'time DESC' }
+      let(:interval) { 60 }
+
+      response '200', 'rates found' do
+        schema type: :object,
+               properties: {
+                 data: {
+                   type: :array,
+                   items: {
+                     type: :object,
+                     properties: {
+                       time: { type: :integer },
+                       low: { type: :decimal },
+                       high: { type: :decimal },
+                       open: { type: :decimal },
+                       close: { type: :decimal },
+                       volume: { type: :decimal }
+                     }
+                   }
+                 },
+                 cursor: {
+                   type: :object,
+                   properties: {
+                     next_page: {
+                       oneOf: [
+                         { type: :string },
+                         { type: :null }
+                       ]
+                     },
+                     previous_page: {
+                       oneOf: [
+                         { type: :string },
+                         { type: :null }
+                       ]
+                     }
+                   }
+                 }
+               }
+        let(:symbols) { 'BTC-USD' }
+
+        run_test!
+      end
+    end
+  end
 end
